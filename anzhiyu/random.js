83667443c2ddbeee0b5dffd1e53ff73544fa775e
FLYFISH <2303402694@qq.com>
2025-07-27 16:32:02 +0800

    Site updated: 2025-07-27 16:32:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/大学/大学/","article/hello-world/","article/学习study/学习/","article/生活/生活/"];function toRandomPost(){
+var posts=["article/hello-world/","article/大学/大学/","article/学习study/学习/","article/生活/生活/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
