efce5fe88ed82570de8109a6b571f0cb8568425f
FLYFISH <2303402694@qq.com>
2026-03-24 14:15:49 +0800

    Site updated: 2026-03-24 14:15:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/hello-world/","article/大学/大学/","article/学习study/学习/","article/生活/生活/"];function toRandomPost(){
+var posts=["article/hello-world/","article/大学/大学/","article/生活/生活/","article/学习study/学习/","article/学习study/WP/[NSSround#28 Team ]Web WP /","article/学习study/WP/商丘师范学院第四届网络安全及信息对抗大赛/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
